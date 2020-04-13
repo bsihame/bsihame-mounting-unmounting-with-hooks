bc2c6d309e3ad7443c16ac63c43a4456c08cd267
bsihame <sihamebazi@pursuit.org>
2020-04-13 18:18:56 -0400

    first commit

---
+++ animals-app/src/AnimalSelectors.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AnimalSelectors = () => {
+  return (
+    <div>
+      display here animals
+    </div>
+  )
+};
+export default AnimalSelectors;
---
+++ animals-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AnimalSelectors from './AnimalSelectors'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <AnimalSelectors/>
+    </div>
+  );
+}
+
+export default App;
